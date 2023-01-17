f8bfa93b9137fa51c5d15ce2f3599e62db4a6fde
James Provance <jb24x7@gmail.com>
2023-01-17 15:06:37 -0800

    created deck of cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,62 @@
 // User Interface Logic
-let form = document.querySelector("form");
+// let form = document.querySelector("form");
 
-function handleForm(event) {
-  event.preventDefault();
-  const userSelections = document.querySelectorAll("input[name=transportation-option]:checked");
-  const userSelectionsArray = Array.from(userSelections);
+// function handleForm(event) {
+//   event.preventDefault();
+//   const userSelections = document.querySelectorAll("input[name=transportation-option]:checked");
+//   const userSelectionsArray = Array.from(userSelections);
 
-  userSelectionsArray.forEach(function(element) {
-    const sortedArray = userSelectionsArray.sort((a, b) => 
-    a.value.localeCompare(b.value));
-    const li = document.createElement("li");
-    li.append(element.value);
-    document.querySelector("ul").append(li);
-  });
+//   userSelectionsArray.forEach(function(element) {
+//     const sortedArray = userSelectionsArray.sort((a, b) => 
+//     a.value.localeCompare(b.value));
+    // const li = document.createElement("li");
+    // li.append(element.value);
+    // document.querySelector("ul").append(li);
+//   });
 
   
+// }
+
+// window.addEventListener("load", function() {
+//   document.querySelector("form#transportation_survey").addEventListener("submit", handleForm);
+// });
+//deck builder
+
+// let deckList = document.getElementById("deck");
+// let suitsArray = [" of clubs", " of hearts", " of diamonds", " of spades"];
+
+// suitsArray.forEach(function() {
+
+// })
+
+var deck = [];
+var suits = ["diamonds", "spades", "hearts", "clubs"];
+var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+
+window.onload = function() {
+
+  suits.forEach(function(suit) {
+    values.forEach(function(value) {
+      deck.push(`${value} of ${suit}`);
+      // const li = document.createElement("li");
+      // li.append(value + " of " + suit);
+      // document.getElementById("deckList").append(li);
+    });
+    
+  });
+
+  deck.forEach(function(element) {
+    const li = document.createElement("li");
+    li.append(element);
+    document.getElementById("deckList").append(li);
+  })
 }
 
-window.addEventListener("load", function() {
-  document.querySelector("form#transportation_survey").addEventListener("submit", handleForm);
-});
+  console.log(deck);
+
+// deck.forEach(function(element) {
+//   const li = document.createElement("li");
+//   li.append(element.value);
+//   document.querySelector("ul").append(li);
+// })
